3f16da04da97b0489512dca3c3352f345a8c796e
Naci Gokmen <nacigokmen99@gmail.com>
2023-09-09 22:39:11 +0200

    Rotasjon er tillagt

---
+++ src/main/java/org/example/Rotasjon.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Rotasjon {
+    public static void main(String[] args) {
+        char[] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        rotasjon(a);
+    }
+
+    public static void rotasjon(char[] a) {
+        if (a.length > 1) {
+            for (int j = a.length - 1; j > 0; j--) {
+                char temp = a[j-1];
+                a[j-1] = a[j ];
+                a[j] = temp;
+                System.out.println(Arrays.toString(a));
+            }
+
+        }
+    }
+}
